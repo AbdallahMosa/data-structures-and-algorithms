b3bcbe77e8b5fff50f6acc306116f01200e404bd
AbdallahMosa <abdallahmosa95@yahoo.com>
2022-08-26 21:48:44 +0300

    done100%

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -148,7 +148,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 
 const reversedString = (str) => {
   // Solution code here...
-  let  = str.split('');
+  let newArray = str.split('');
   let reverse = newArray.reduce((acc, val) => {
       return val + acc;
   }, '')
